2b67823c7f29f267b2fdeed4b50f1816de7d49df
Sami Laine <sami.v.laine@gmail.com>
2021-02-10 18:44:31 +0100

    Add pin cog

---
+++ bot/cogs/pin.py
@@ -0,0 +1,29 @@
+from typing import Dict, Optional
+
+from discord.channel import TextChannel
+from bot import LogBot
+from discord import Message
+from discord.ext import commands
+
+
+class Pin(commands.Cog):
+    def __init__(self, bot: LogBot) -> None:
+        self.bot = bot
+        self.config: Dict = self.bot.config['cogs']['pin']
+        self.keyword: str = self.config['keyword']
+        self.channel: Optional[TextChannel] = None
+
+    async def init(self):
+        self.channel = await self.bot.fetch_channel(self.config['channel'])
+
+    @commands.Cog.listener()
+    async def on_message(self, message: Message):
+        if message.channel != self.channel:
+            # we only care about messages in the suggestion channel
+            return
+        if self.keyword in message.content:
+            await message.pin(reason="Automatic suggestion pin")
+
+
+def setup(bot: LogBot):
+    bot.add_cog(Pin(bot))
