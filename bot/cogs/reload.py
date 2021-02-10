9fca584eb5bbac0151e68c57b35c901692a4fa77
Sami Laine <sami.v.laine@gmail.com>
2021-02-10 20:13:03 +0100

    Simplify reload

--- bot/cogs/reload.py
+++ bot/cogs/reload.py
@@ -11,26 +11,17 @@ class Reload(Cog):
     @command()
     async def reload(self, ctx: Context):
         print("Reloading extensions")
+
         unloaded = []
         for extension in self.extensions:
-            try:
-                self.bot.unload_extension(extension)
-                print("unloaded", extension)
-            except ExtensionNotLoaded:
-                await ctx.send("Skipping unload for not loaded extension {}"
-                               .format(extension))
-            else:
+            if await self._unload_extension(ctx, extension):
                 unloaded.append(extension)
+
         loaded = []
         for extension in self.extensions:
-            try:
-                self.bot.load_extension(extension)
-                print("loaded", extension)
-            except Exception:
-                await ctx.send("An error occured while reloading: ```{}```"
-                               .format(traceback.format_exc()))
-            else:
+            if await self._load_extension(ctx, extension):
                 loaded.append(extension)
+
         if len(loaded) > 0:
             await ctx.send(
                 "Reloaded following extensions: {}".format(loaded))
@@ -41,6 +32,28 @@ class Reload(Cog):
         for cog in self.bot.cogs.values():
             if "init" in dir(cog):
                 await cog.init()
+
+    async def _unload_extension(self, ctx, extension):
+        try:
+            self.bot.unload_extension(extension)
+            print("unloaded", extension)
+        except ExtensionNotLoaded:
+            await ctx.send("Skipping unload for not loaded extension {}"
+                           .format(extension))
+            return False
+        else:
+            return True
+
+    async def _load_extension(self, ctx, extension):
+        try:
+            self.bot.load_extension(extension)
+            print("loaded", extension)
+        except Exception:
+            await ctx.send("An error occured while reloading: ```{}```"
+                           .format(traceback.format_exc()))
+            return False
+        else:
+            return True
 
 
 def setup(bot):
